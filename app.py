c3c94b8fb7908b52e23c0cb203bbad99d4721a9c
Kelly Caylor <kelly@pulsepod.io>
2014-10-01 13:13:51 -0400

    basic setup is there

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-from flask import Flask
-from dataset import Dataset
-from pymongo import MongoClient
+from flask import Flask, abort
+from dataset import Dataset, database_list
 
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017/')
-opendap_databases = client['opendap_databases']
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def opendap_json(url_dataset=None, lat=None, lon=None):
+    # dataset = opendap_database_list.find(name=url_dataset).first()?
+    if not url_dataset:
+        abort(404)
+    else:
+        database_obj = database_list[url_dataset]
+        database_obj.get_xy()
+        database_obj.get_data()
+        database_obj.make_json()
+        return "Hello World!"
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
 
--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,12 +1,21 @@
-from app import opendap_databases
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+opendap_database = client['opendap_databases']
+
+opendap_database_list = opendap_database['database_list']
+
+# for item in opendap_database_list:
+#   this_database = database_init(item.name)
+#   database_list[item.name] = this_database
+
 
 class Dataset(object):
     """docstring for Dataset"""
-    def __init__(self, arg):
+    def __init__(self):
         super(Dataset, self).__init__()
-        self.arg = arg
         self.name = 'vic_conus_3km'
-        self.collection = opendap_databases[self.name]
+        self.collection = opendap_database[self.name]
 
     def get_xy(self,lat=None,lon=None):
         # Do the search
@@ -18,6 +27,9 @@ class Dataset(object):
     def make_json(self):
         pass
 
-
+# This will end up somewhere else...
+database_list = {}
+my_dataset = Dataset()
+database_list[my_dataset.name] = my_dataset
 
         
