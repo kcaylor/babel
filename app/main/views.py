27532a3f9e42f38c5616e9c100e220ebcbbb859f
Kelly Caylor <kelly@pulsepod.io>
2014-10-14 07:46:07 -0400

    working get_data

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,13 @@
 from flask import abort, jsonify
 from . import main
 from app.models import Dataset
+import json
+import pydap.client
+
 
 dataset = Dataset.objects(name='vic_conus_3km').first()
+dataset.data = pydap.client.open_url(dataset.url)
+
 
 @main.route("/")
 def hello():
@@ -21,6 +26,27 @@ def opendap_json(api_key=None, location=None):
     except:
         abort(404)
     if lat and lon:
+        location = json.loads(dataset.get_xy(lat=lat,lon=lon))
+    else:
+        return "error: provide latitude and longitude"
+    result = 'dummy ' + str(lat) + str(lon)
+    #result = str(dataset.get_data(x=location[0]['x'],y=location[0]['y']))
+    return result
+
+
+@main.route("/latlon/<location>", methods=['GET'])
+def latlon_json(location=None):
+    try:
+        loc = location.split(',')
+    except:
+        abort(404)
+    try:
+        lat = float(loc[0])
+        lon = float(loc[1])
+    except:
+        abort(404)
+    if lat and lon:
         return dataset.get_xy(lat=lat,lon=lon)
     else:
         return "error: provide latitude and longitude"
+    
--- app/models/dataset.py
+++ app/models/dataset.py
@@ -19,7 +19,10 @@ class Vic_Conus_3km(db.Document):
 class Dataset(db.Document):
 
     name = db.StringField(db_field='name')
-    
+    # the precision of the data should be defined, otherwise 0 becomes 0.0000000000something
+    precision = db.IntField(default=8)
+    url = db.StringField()
+
     meta = {
         'collection': 'dataset_list',
         'indexes': [
@@ -27,18 +30,27 @@ class Dataset(db.Document):
         ]
     }
 
+
     def __repr__(self):
         return '<Dataset %r>' % self.name
 
     def get_id(self):
         return unicode(self.id)
 
-    def get_xy(self,lat=None,lon=None,limit=1):
+    def get_xy(self,lat=None,lon=None,limit=100):
         point = Vic_Conus_3km.objects(point__near=[lon,lat]).limit(limit)
         return point.to_json()
 
-    def get_data(self,x=None,y=None):
-        pass
+    def get_data(self,x=None,y=None,var=None):
+        if not var:
+            var = 'prec'
+        # var should be passed together with x and y
+        # time must be passed too, but so far we just pick all the data from July 1st, 2009
+        subset = self.data[var][var][1,y,x][0]*self.data[var].scale_factor+self.data[var].add_offset
+        # the type of the output must be defined, so far it is numpy.ndarray
+        # NoData must be managed too, they are defined as data[var]._FillValue, maybe they can be returned as a separate variable
+        return subset.round(self.precision) #, data[var]._FillValue
+ 
 
     def make_json(self):
         pass
